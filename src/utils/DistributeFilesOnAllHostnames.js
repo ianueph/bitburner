f88a7a9052a64085899d4f8c0f73867b9c3e4576
ianueph <111487555+ianueph@users.noreply.github.com>
2024-07-30 09:44:15 +0800

    Updated terminal print

--- src/utils/DistributeFilesOnAllHostnames.js
+++ src/utils/DistributeFilesOnAllHostnames.js
@@ -1,10 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
     const hostnames = ns.read("UniqueHostnames.txt").split(",");
-    const scripts = ns.ls("home").filter((file) => file.endsWith(".js"));
+    const scripts = ns.ls("home").filter((file) => file.endsWith(".js"))
   
     for (const hostname of hostnames) {
       ns.scp(scripts, hostname, "home")
-      ns.tprint(hostname, " \t\t\t", ns.getServerMaxRam(hostname), ns.hasRootAccess(hostname))
+      ns.tprint("SUCCESS distributed files from home to ", hostname)
     }
 }
